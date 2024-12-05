f304111b8c7cfdb2cffbec0fce29cdefde02ea36
Bibek Shrestha <bibek@192.168.1.10>
2024-12-05 14:53:46 +0545

    [FIX] issue with git

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,13 @@ app.use(bodyparser.json());
 
 app.use("/admin/auth", adminRoutes);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
 mongoose
   .connect(process.env.DB_URL)
   .then((result) => {
@@ -18,3 +25,4 @@ mongoose
     console.log("Database connection ready!");
   })
   .catch((err) => console.error("Failed to connect to the database:", err));
+
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const { body } = require("express-validator");
+
+const Admin = require("../models/admin");
 
 const router = express.Router();
 
-router.post("/signup");
+router.post("/signup", [
+  body("email")
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .custom(async (value) => {
+      const userDoc = await Admin.findOne({ email: value });
+      if (userDoc) {
+        return Promise.reject("E-Mail address already exists!");
+      }
+    })
+    .normalizeEmail(),
+  body("password").trim().isLength({ min: 5 }),
+  body("name").trim().not().isEmpty(),
+]);
 
 module.exports = router;
